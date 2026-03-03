9b728a8edb6c08e7be120c8c073564eb343f58ba
Robin Louiset <robinlouiset@mbp-de-robin.home>
2026-03-03 16:29:04 +0100

    Update avmnist

--- av_mnist/avmnist_sum.py
+++ av_mnist/avmnist_sum.py
@@ -340,21 +340,14 @@ def mnist(args):
     # Compute PointWise PID
     list_of_pointwise_pid = []
     for j_i, m0_i, m1_i, r_i, y_i in zip(joint_pred, img_pred, aud_pred, y_pred_dict["average"], y_pred_dict["targets"]):
-
-        print(r_i)
-        print(m0_i)
-        print(m1_i)
-        print(j_i)
-        print(debug)
-
         y_i = y_i.long()
 
-        total_contribution = 0.593 + torch.log(torch.clamp(j_i, min=1e-12, max=1.0))[y_i]
+        total_contribution = 0.593 + torch.log(torch.clamp(softmax(j_i), min=1e-12, max=1.0))[y_i]
 
         r_contribution = 0.593 + torch.log(torch.clamp(softmax(r_i), min=1e-12, max=1.0))[y_i]
 
-        m0_contribution = 0.593 + torch.log(torch.clamp(m0_i, min=1e-12, max=1.0))[y_i] - r_contribution
-        m1_contribution = 0.593 + torch.log(torch.clamp(m1_i, min=1e-12, max=1.0))[y_i] - r_contribution
+        m0_contribution = 0.593 + torch.log(torch.clamp(softmax(m0_i), min=1e-12, max=1.0))[y_i] - r_contribution
+        m1_contribution = 0.593 + torch.log(torch.clamp(softmax(m1_i), min=1e-12, max=1.0))[y_i] - r_contribution
 
         s_contribution = total_contribution - m0_contribution - m1_contribution - r_contribution
 
