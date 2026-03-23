d39beec79cadc1053e597f3290b82d7023c78ed8
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2026-03-23 15:46:46 +0100

    AVMNIST sum experiment

--- av_mnist/avmnist_sum_update.py
+++ av_mnist/avmnist_sum_update.py
@@ -276,33 +276,33 @@ def compute_pointwise_pid_from_probs(dict_of_metrics, num_classes):
         h_y = -log_py_i
 
         # ===== CLIPPING =====
-        modality0_ce = min(modality0_ce, h_y)
-        modality1_ce = min(modality1_ce, h_y)
+        """modality0_ce = min(modality0_ce, h_y)
+        modality1_ce = min(modality1_ce, h_y)"""
         redundancy_ce = min(redundancy_ce, h_y)
 
-        redundancy_ce = max(redundancy_ce, joint_ce, modality0_ce, modality1_ce)
+        redundancy_ce = max(redundancy_ce, joint_ce)
 
         modality0_ce = max(modality0_ce, joint_ce)
         modality1_ce = max(modality1_ce, joint_ce)
 
-        modality0_ce = min(modality0_ce, redundancy_ce)
-        modality1_ce = min(modality1_ce, redundancy_ce)
+        """modality0_ce = min(modality0_ce, redundancy_ce)
+        modality1_ce = min(modality1_ce, redundancy_ce)"""
 
         # ===== INFORMATION =====
         total = h_y - joint_ce
 
         r_val = h_y - redundancy_ce
 
-        u0 = max(0, h_y - modality0_ce - r_val)
-        u1 = max(0, h_y - modality1_ce - r_val)
+        u0 = h_y - modality0_ce - r_val
+        u1 = h_y - modality1_ce - r_val
 
         s = total - u0 - u1 - r_val
 
-        if s < 0:
+        """if s < 0:
             r_val -= s
             u0 = max(0, h_y - modality0_ce - r_val)
             u1 = max(0, h_y - modality1_ce - r_val)
-            s = 0
+            s = 0"""
 
         pid_list.append([u0, u1, r_val, s])
 
