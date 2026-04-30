cb262b0a1fc97bc1fe42a034fecff1db678befba
Robin Louiset <robinlouiset@mbp-de-robin.home>
2026-04-30 15:20:31 +0200

    avmnist sum

--- av_mnist/avmnist_sum_lsmi.py
+++ av_mnist/avmnist_sum_lsmi.py
@@ -593,7 +593,7 @@ def mnist(args):
     # =======================
     # 1. DATA
     # =======================
-    cutoff_sum = 6
+    cutoff_sum = 7
     AV_train, AV_test = prepare_dataset(args, cutoff_sum=cutoff_sum)
 
     # =======================
