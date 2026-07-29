20d3390fd4acf61a8d46625565158d47eaa7b376
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2026-07-29 16:39:02 +0200

    hp_sweep

--- av_mnist/avmnist.py
+++ av_mnist/avmnist.py
@@ -610,7 +610,7 @@ def mnist(args):
         y_test,
         "redundancy",
         distribution_target="categorical",
-        lambda_reg=25,
+        lambda_reg=50,
         num_classes=10,
         lr=1e-4
     )
