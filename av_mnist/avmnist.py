d5b9e8aaa6c211bd4a4ad9ce2d2e420ee1b57f0d
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2026-09-01 14:40:19 +0200

    avmnist

--- av_mnist/avmnist.py
+++ av_mnist/avmnist.py
@@ -641,7 +641,7 @@ def mnist(args):
         y_test,
         "redundancy",
         distribution_target="categorical",
-        lambda_reg=10,
+        lambda_reg=50,
         num_classes=10,
         lr=1e-4
     )
